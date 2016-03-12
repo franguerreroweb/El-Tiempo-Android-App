e318a1204372771ed115123fc3d1ef7472e619b1
franguerreroweb <franguerreroweb@gmail.com>
2016-03-12 19:48:48 +0100

    Previsión del Tiempo

---
+++ src/main/java/franguerrero/tiempo/HandleXML.java
@@ -0,0 +1,133 @@
+package franguerrero.tiempo;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HandleXML {
+    private String country = "county";
+    private String temperature = "temperature";
+    private String humidity = "humidity";
+    private String pressure = "pressure";
+    private String urlString = null;
+    private XmlPullParserFactory xmlFactoryObject;
+    public volatile boolean parsingComplete = true;
+
+    public HandleXML(String url){
+        this.urlString = url;
+    }
+
+    public String getCountry(){
+        return country;
+    }
+
+    public String getTemperature(){
+        return temperature;
+    }
+
+    public String getHumidity(){
+        return humidity;
+    }
+
+    public String getPressure(){
+        return pressure;
+    }
+
+    public void parseXMLAndStoreIt(XmlPullParser myParser) {
+        int event;
+        String text=null;
+        try {
+            event = myParser.getEventType();
+            while (event != XmlPullParser.END_DOCUMENT) {
+                String name=myParser.getName();
+                switch (event){
+
+                    case XmlPullParser.START_TAG:
+
+                        if ( !name.equals("country"))
+                            text = myParser.getText();
+
+                        if(name.equals("humidity")){
+                            humidity = "Humedad: ";
+                            humidity += myParser.getAttributeValue(null,"value");
+                            humidity += myParser.getAttributeValue(null,"unit");
+                        }
+                        else if(name.equals("pressure")){
+                            pressure = "Presión: ";
+                            pressure += myParser.getAttributeValue(null,"value");
+                            pressure += myParser.getAttributeValue(null,"unit");
+                        }
+                        else if(name.equals("temperature")){
+                            temperature = "Temperatura: ";
+                            temperature += myParser.getAttributeValue(null,"value");
+                            temperature += "º";
+                        }
+                        else if(name.equals("weather")){
+                            country = "Hoy: ";
+                            country += myParser.getAttributeValue(null,"value");
+                        }
+                        else{
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+
+                        text = myParser.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+
+                        if(name.equals("country")){
+                            //country = text;
+                        }
+                        else {
+
+                        }
+                        break;
+
+
+                }
+                event = myParser.next();
+            }
+            parsingComplete = false;
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fetchXML(){
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+                    conn.setReadTimeout(10000 /* milliseconds */);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream stream = conn.getInputStream();
+                    xmlFactoryObject = XmlPullParserFactory.newInstance();
+                    XmlPullParser myparser = xmlFactoryObject.newPullParser();
+
+                    myparser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                    myparser.setInput(stream, null);
+
+                    parseXMLAndStoreIt(myparser);
+                    stream.close();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
---
+++ src/main/java/franguerrero/tiempo/MainActivity.java
@@ -0,0 +1,84 @@
+package franguerrero.tiempo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText ed1,ed2,ed3,ed4,ed5;
+
+    private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private String url2 = "&mode=xml";
+    private String url3 = "&units=metric";
+    private String url4 = "&lang=es";
+    private String url5 = "&appid=1140bcd59f8c8e6b148bbdacf17bc8cf";
+    private HandleXML obj;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        b1=(Button)findViewById(R.id.button);
+
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+        ed3=(EditText)findViewById(R.id.editText3);
+        ed4=(EditText)findViewById(R.id.editText4);
+        ed5=(EditText)findViewById(R.id.editText5);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ed1.getText().toString();
+                String finalUrl = url1 + url + url2 + url3 + url4 + url5;
+                ed2.setText(finalUrl);
+
+                obj = new HandleXML(finalUrl);
+                obj.fetchXML();
+
+                while (obj.parsingComplete) ;
+                ed2.setText(obj.getCountry());
+                ed3.setText(obj.getTemperature());
+                ed4.setText(obj.getHumidity());
+                ed5.setText(obj.getPressure());
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
